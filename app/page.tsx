2df3890f4a5b3e28f2e61e7565a61dc150ae8007
Shubham Sharma <shubhamsh3099@gmail.com>
2026-08-30 20:29:32 +0530

    remove Idea section component from landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import HeroSection from "@/components/HeroSection";
-import IdeaSection from "@/components/IdeaSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      <IdeaSection />
     </>
   )
 }
