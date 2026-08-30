503ed92fb2c126e678f8e929ec96739cef55e51b
Shubham Sharma <shubhamsh3099@gmail.com>
2026-08-30 20:28:38 +0530

    remove space division component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import HeroSection from "@/components/HeroSection";
 import IdeaSection from "@/components/IdeaSection";
-import SpaceDivision from "@/components/SpaceDivision";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      <SpaceDivision />
       <IdeaSection />
     </>
   )
--- components/SpaceDivision.tsx
+++
@@ -1,5 +0,0 @@
-export default function SpaceDivision() {
-    return (
-        <div className="h-[50px]"></div>
-    )
-}
