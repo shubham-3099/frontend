38395bb07cf4ecb9a3c9f185198976721b9475e2
Shubham Sharma <shubhamsh3099@gmail.com>
2026-08-30 19:47:21 +0530

    remove vlogger section from landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import HeroSection from "@/components/HeroSection";
 import IdeaSection from "@/components/IdeaSection";
-import VloggerSection from "@/components/VloggerSection";
 import SpaceDivision from "@/components/SpaceDivision";
 
 export default function Home() {
@@ -9,7 +8,6 @@ export default function Home() {
       <HeroSection />
       <SpaceDivision />
       <IdeaSection />
-      <VloggerSection />
     </>
   )
 }
