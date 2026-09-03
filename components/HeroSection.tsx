3047c7f72cd8c0af47520db52cbad17a9e13da5e
Shubham Sharma <shubhamsh3099@gmail.com>
2026-09-03 13:16:16 +0530

    Add link to hero section buttons to home page

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,7 +1,10 @@
+'use client'
 import IdeaFeature from "./IdeaFeature";
 import { ChevronRight, MapPin, Gem, Image as ImageIcon, MessagesSquare } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function HeroSection() {
+    const router = useRouter();
     return (
         <div className="flex flex-col items-center lg:flex-row md:justify-center my-10 md:my-25 md:px-20 gap-10">
             <div className="flex flex-col gap-8 justify-center w-[340px] md:w-[600px] md:mr-40">
@@ -14,8 +17,8 @@ export default function HeroSection() {
                     <IdeaFeature icon={MapPin} text="Location-based results near you" />
                 </div>
                 <div className="flex gap-4 pb-5">
-                    <button className="border px-4 py-2"> Search </button>
-                    <button className="flex items-center"> Explore <ChevronRight className="w-5 h-4 mt-1" /> </button>
+                    <button onClick={() => router.push("/home")} className="border px-4 py-2"> Search </button>
+                    <button onClick={() => router.push("/home")} className="flex items-center"> Explore <ChevronRight className="w-5 h-4 mt-1" /> </button>
                 </div>
             </div>
             <img src="/food/image-1.jpg" alt="" className="max-w-[340px] md:max-w-[700px] md:h-[600px]" />
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,14 @@
+'use client'
 import Image from "next/image";
 import { Menu } from "lucide-react";
-import { ChevronDown } from "lucide-react";
+import {useRouter} from "next/navigation";
 
 export default function Navbar() {
+    const router = useRouter();
     return (
         <nav className="flex justify-between md:px-12 px-5 items-center h-18">
             <Image src="/logo.png" alt="Logo of the company" width={100} height={70} />
-            <button className="hidden md:flex bg-black text-white py-2 px-5">Sign up</button>
+            <button onClick={() => router.push("/home")} className="hidden md:flex bg-black text-white py-2 px-5">Sign up</button>
             <Menu className="md:hidden"/>
         </nav>
     )
